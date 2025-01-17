491fbc1c2378a2260002683634c208d7ac170dd2
HeeJung <est4231@gmail.com>
2025-01-17 14:40:27 +0900

    click_screen.dart 추가

--- online_lecture_smwu/lib/screen/button_screen.dart
+++ online_lecture_smwu/lib/screen/button_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_lecture_smwu/screen/click/click_screen.dart';
 import 'package:online_lecture_smwu/screen/column_practice_screen.dart';
 import 'package:online_lecture_smwu/screen/column_row_practice_screen.dart';
 import 'package:online_lecture_smwu/screen/column_screen.dart';
@@ -10,10 +11,12 @@ import 'package:online_lecture_smwu/screen/row/row_practice_screen.dart';
 import 'package:online_lecture_smwu/screen/row/row_screen.dart';
 import 'package:online_lecture_smwu/screen/stack/stack_practice_screen.dart';
 import 'package:online_lecture_smwu/screen/stack/stack_screen.dart';
+import 'package:online_lecture_smwu/screen/stateless/stateless_screen.dart';
 import 'package:online_lecture_smwu/screen/text/text_screen.dart';
 
 import 'listview/listview_builder_practice_screen.dart';
 import 'listview/listview_builder_screen.dart';
+import 'stateful/stateful_screen.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({super.key});
@@ -170,6 +173,36 @@ class ButtonScreen extends StatelessWidget {
                     ));
                   },
                   child: const Text("ListviewBuilder 실습")),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return const StatelessScreen();
+                      },
+                    ));
+                  },
+                  child: const Text("Stateless")),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return const StatefulScreen();
+                      },
+                    ));
+                  },
+                  child: const Text("Stateful")),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return const ClickScreen();
+                      },
+                    ));
+                  },
+                  child: const Text("Click")),
             ],
           ),
         ),
